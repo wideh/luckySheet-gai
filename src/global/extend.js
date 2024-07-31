b3d23dee1adbb6858c3ece2f4b28ce8116cca3bb
yangxin <255@qq.com>
2024-07-31 13:33:29 +0800

    增加行或列时，保留selfType

--- src/global/extend.js
+++ src/global/extend.js
@@ -635,6 +635,11 @@ function luckysheetextendtable(type, index, value, direction, sheetIndex) {
                             range: emptyRange,
                         };
 
+                        if(cfg["borderInfo"][i]?.selfType) {
+                            console.log('增加行变化', emptyRange);
+                            bd_obj.selfType = cfg["borderInfo"][i]?.selfType;
+                        }
+
                         borderInfo.push(bd_obj);
                     }
                 } else if (rangeType == "cell") {
@@ -791,6 +796,11 @@ function luckysheetextendtable(type, index, value, direction, sheetIndex) {
                             color: cfg["borderInfo"][i].color,
                             range: emptyRange,
                         };
+
+                        if(cfg["borderInfo"][i]?.selfType) {
+                            console.log('增加列变化', emptyRange);
+                            bd_obj.selfType = cfg["borderInfo"][i]?.selfType;
+                        }
 
                         borderInfo.push(bd_obj);
                     }
